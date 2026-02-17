7dc1787cf57fda8f98aed7db5f591e484cc3c648
Yhojan Caro“ <caroyhojan@gmail.com>
2026-02-16 20:41:58 -0500

    Clase_4_Agenda_ADSO_v2

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import "./App.css";
+import ContactoCard from "./components/ContactoCard";
+import FormularioContacto from "./components/FormularioContacto";
+export default function App() {
+const [contactos, setContactos] = useState([
+{
+id: 1,
+nombre: "Carolina Pérez",
+telefono: "300 123 4567",
+correo: "carolina@sena.edu.co",
+etiqueta: "Compañera",
+},
+]);
+const agregarContacto = (nuevo) => {
+setContactos((prev) => [...prev, { id: Date.now(), ...nuevo }]);
+};
+const eliminarContacto = (id) => {
+setContactos((prev) => prev.filter((c) => c.id !== id));
+};
+
+return (
+<main className="app-container">
+<h1 className="app-title">Agenda ADSO v2</h1>
+<FormularioContacto onAgregar={agregarContacto} />
+<section className="lista-contactos">
+{contactos.map((c) => (
+<ContactoCard
+key={c.id}
+id={c.id}
+nombre={c.nombre}
+telefono={c.telefono}
+correo={c.correo}
+etiqueta={c.etiqueta}
+onDelete={eliminarContacto}
+/>
+))}
+</section>
+</main>
+);
+}
